b34e773692cf7423c5f67022d6fd983efdfeac7f
Bjornar <bjornp11@uia.no>
2022-10-28 19:19:39 +0200

    Initial commit UI_mock.py

---
+++ code/UI_mock.py
@@ -0,0 +1,13 @@
+
+def getUserInput():
+    userinput = {
+        "PriceArea": "NO5",
+        "Year": "2022",
+        "Month": "10",
+        "Day": "27"
+    }
+    return userinput
+
+
+if __name__ == '__main__':
+    print(getUserInput())
