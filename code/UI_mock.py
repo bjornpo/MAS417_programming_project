b2f260bf87a567c4c596b7cd14bdb5616160397c
ninagbr <97841460+ninagbr@users.noreply.github.com>
2022-10-30 20:12:40 +0100

    Update UI_mock.py
    just added some comments

--- code/UI_mock.py
+++ code/UI_mock.py
@@ -1,10 +1,10 @@
 
 def getUserInput():
     userinput = {
-        "PriceArea": "NO5",
-        "Year": "2022",
-        "Month": "10",
-        "Day": "27"
+        "PriceArea": "NO5",         #NO1/NO2/NO3/NO4/NO5
+        "Year": "2022",             #Always four digits
+        "Month": "10",              #Always two digits, first digit 0 
+        "Day": "27"                 #Always two digits, first digit 0 
     }
     return userinput
 
