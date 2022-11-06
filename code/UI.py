eb2ed049f1d865d1d43697aa0c04f1ebc154e6fb
ninagbr <97841460+ninagbr@users.noreply.github.com>
2022-11-06 19:14:30 +0100

    Update UI.py

--- code/UI.py
+++ code/UI.py
@@ -1,29 +1,31 @@
-from datetime import date
 
 
 def getUserInput():
-    # get the date
+    
+    #get the date
     dateInput = input("Select Date (year-mo-day): ").split('-')
-    print(dateInput)
+    print(dateInput)    
     year, month, day = [int(item) for item in dateInput]
-
-    # get the area
+    
+    #get the area
     print("NO1 = Oslo\ Øst-Norge")
-    print("NO2 = Kristiansand / Sør-Norge")
+    print("NO2 = Kristiansand / Sør-Norge" )
     print("NO3 = Trondheim / Midt-Norge")
-    print("NO4 = Tromsø / Nord-Norge")
+    print("NO4 = Tromsø / Nord-Norge") 
     print("NO5 = Bergen / Vest-Norge): ")
-    area = input("Select Area: ")
-
-    userinput = {
-        "PriceArea": area,  # NO1/NO2/NO3/NO4/NO5
-        "Year": year,  # Always four digits
-        "Month": month,  # Always two digits, first digit 0
-        "Day": day  # Always two digits, first digit 0
+    area = input ("Select Area: ")
+    
+    userInput = {
+        "PriceArea": area,         #NO1/NO2/NO3/NO4/NO5
+        "Year": year,             #Always four digits
+        "Month": month,              #Always two digits, first digit 0 
+        "Day": day                 #Always two digits, first digit 0 
     }
-    return userinput
+    return userInput
+
+
 
 
 if __name__ == '__main__':
-    print(getUserInput())
+   userInput = getUserInput()
 
