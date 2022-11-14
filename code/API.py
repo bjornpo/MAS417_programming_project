e031e44c932102503814545ce11b81905bd0ef2b
ninagbr <97841460+ninagbr@users.noreply.github.com>
2022-11-14 09:43:15 +0100

    updating API

--- code/API.py
+++ code/API.py
@@ -1,12 +1,6 @@
 #import json
 import requests
 
-dummyUI = {
-    "PriceArea": "NO5",
-    "Year": "2022",
-    "Month": "10",
-    "Day": "27"
-}
 
 
 def createRequestLink(parameters):
@@ -17,19 +11,19 @@ def createRequestLink(parameters):
     link =    "https://www.hvakosterstrommen.no/api/v1/prices/{}/{}-{}_{}.json".format(year, month,day,area)
     #print (link)
     return link
-
-
-
+    
+    
 
 def getElectricityPrice(userInput):
     requestLink = createRequestLink(userInput);
     response = requests.get(requestLink)  #gets the status code from the request
-    #print(response.status_code) 
+    statusCode = response.status_code
     jsonData = response.json()  #returns the json file contents from the response in a dict format
-    #print(jsonData)
-    return jsonData
-  
     
+    info = [jsonData, statusCode]
+    return info
+  
+
 
 if __name__ == '__main__':
-    print(getElectricityPrice(dummyUI))
+    getElectricityPrice()
