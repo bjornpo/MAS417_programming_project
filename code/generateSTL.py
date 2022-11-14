52b530d38f6af19860dde964753a0d09dbbfba47
Bjornar <bjornp11@uia.no>
2022-11-14 23:53:25 +0100

    Fixing code

--- code/generateSTL.py
+++ code/generateSTL.py
@@ -1,78 +1,107 @@
-import requests
-import numpy
-from stl import mesh
+import cadquery as cq
+import math
+from cadquery import exporters
+from pathlib import Path
 
+def generateSTL(userInput, APIdata):
 
-def generateSTL():
+    baseWidth = 170
+    baseDepth = 45
+    baseHeigth = 40
+    baseTh = 2
+    barSenterToEdge = 5
+    barDepth = 8
+    barWidth = 5
+
+    try:
+        highestPrice = max([x['NOK_per_kWh'] for x in APIdata])
+    except:
+        highestPrice = 3.0
+        print(f'Unable to find maximum price. assume {highestPrice}kr')
+
+    #Scale the height of the barchart based on maximum price
+    if highestPrice < 1.0:
+        Hfactor = 100 #mm barheight pr. krone
+    elif highestPrice < 2.5:
+        Hfactor = 50
+    elif highestPrice < 5.0:
+        Hfactor = 25
+    else:
+        Hfactor = 20
+
+    year = userInput.get('Year', "YYYY")
+    month = userInput.get('Month', "MM")
+    day = userInput.get('Day', "DD")
+    priceZone = userInput.get('PriceArea', "NOX")
+
+    barSpacing = (baseWidth - barSenterToEdge*2)/23
+    angle = math.degrees(math.atan((baseDepth-barDepth) / (baseHeigth-baseTh)))
+
+    print("Generating stl-file. This may take several minutes")
+
+    model = (
+        cq.Workplane("YZ")
+        .lineTo(baseDepth, 0)
+        .lineTo(baseDepth, baseTh)
+        .lineTo(barDepth, baseHeigth)
+        .lineTo(0, baseHeigth)
+        .close()
+        .extrude(-baseWidth)
+    )
+
+    model = ( #Find workplane for nameplate
+            model.faces(">Z").workplane(origin=(0,barDepth))
+            .transformed(offset=cq.Vector(0, 0, 0), rotate=cq.Vector(angle-90,0,180))
+            .tag("nameplate")
+    )
+
+    model = ( #Generating bar timestamp
+            model.workplaneFromTagged("nameplate")
+            .center(barSenterToEdge-barSpacing,-2)
+            .transformed(offset=cq.Vector(0, 0, 0), rotate=cq.Vector(0,0,90))
+            )
+    #for h in range(1): #speed up processing time when testing
+    for h, item in enumerate(APIdata):
+        text = f'{h:02d}-{h+1:02d}'
+        model = model.center(0,-barSpacing)
+        model = model.text(txt=text, fontsize=barSpacing-0, distance=-0.5, halign="right", valign="center")
+
+    model = ( #generating text for price-zone and date
+            model.workplaneFromTagged("nameplate")
+            .center(5 ,-50)
+            #.text(txt="12-11-2022", fontsize=15, distance=-1, halign="left", valign="bottom")
+            .text(txt=f'{day}-{month}-{year}', fontsize=15, distance=-1, halign="left", valign="bottom")
+            .workplaneFromTagged("nameplate")
+            .center(baseWidth-5 ,-50)
+            .text(txt=f'{priceZone}', fontsize=15, distance=-1, halign="right", valign="bottom")
+            .workplaneFromTagged("nameplate")
+            .center(baseWidth/2,-35)
+            .text(txt=f'Spot-price electricity (1kr = {Hfactor}mm)', fontsize=10, distance=-1, halign="center", valign="bottom")
+            )
+
+    #generate bars
+    model = model.faces(">Z").workplane(origin=(-barSenterToEdge+barSpacing,barDepth/2))#.box(10,10,10)
+    #for h in range(1): #speed up time during testing
+    for h, item in enumerate(APIdata):
+        try:
+            price = APIdata[h]["NOK_per_kWh"]
+        except:
+            price = 0.001
+            print("Error getting value from API-data")
+
+        model = model.center(-barSpacing,0).box(barWidth, barDepth, price*Hfactor, centered=[True,True,False])
+
+    #export stl-file
+    filePath = Path().cwd() / 'stl_files'
+    filePath.mkdir(parents=True, exist_ok=True) #Create directory if it not exsists
+    filename = f'{year}_{month}_{day}_{priceZone}.stl'
+    exporters.export(model, str(filePath / filename))
+
     fileInfo = {
-        "fileName": "Filename",
-        "filePath": "FilePath"
+        "fileName": str(filename),
+        "filePath": str(filePath)
     }
     return fileInfo
 
-
-def createRequestLink(parameters):
-    year = parameters["Year"]
-    month = parameters["Month"]
-    day = parameters["Day"]
-    area = parameters["PriceArea"]
-    link = "https://www.hvakosterstrommen.no/api/v1/prices/{}/{}-{}_{}.json".format(year, month, day, area)
-    #print (link)
-    return link
-
-
-def getElectricityPrice(userInput):
-    requestLink = createRequestLink(userInput)
-    response = requests.get(requestLink)  #gets the status code from the request
-    #print(response.status_code)
-    jsonData = response.json()  #returns the json file contents from the response in a dict format
-    #print(response.json()[0])
-    return jsonData
-
-
 if __name__ == '__main__':
-
-    #UI = {
-    #    "PriceArea": input("Price Area: "),
-    #    "Year": input("Year: "),
-    #    "Month": input("Month: "),
-     #   "Day": input("Day: ")
-    #}
-
-    dummyUI = {
-        "PriceArea": "NO5",
-        "Year": "2022",
-        "Month": "10",
-        "Day": "27"
-    }
-
-    jsonData = getElectricityPrice(dummyUI)
-
-    NOK_per_kWh = []
-
-    for index, item in enumerate(jsonData):
-        NOK_per_kWh.append(jsonData[index]["NOK_per_kWh"])
-
-    print(NOK_per_kWh)
-
-    vertices = numpy.empty((0, 3), float)
-    faces = numpy.empty((0, 3), int)
-
-    for index, item in enumerate(jsonData):
-        vertices = numpy.append(vertices, [[index, 0, 0]], axis=0)
-        vertices = numpy.append(vertices, [[index, 10*NOK_per_kWh[index],0]], axis=0)
-
-
-    for index, item in enumerate(vertices):
-        faces = numpy.append(faces, [[index-2, index -1, index]], axis=0)
-        #faces = numpy.append(faces, [[index + 5, index + 4, index + 3]], axis=0)
-    print(vertices)
-
-    cube = mesh.Mesh(numpy.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
-    for i, f in enumerate(faces):
-        for j in range(3):
-            cube.vectors[i][j] = vertices[f[j],:]
-
-    # Write the mesh to file "cube.stl"
-    cube.normals
-    cube.save('cube.stl')
+    generateSTL({}, [{}])
